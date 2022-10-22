807b530943580ed59a9e470b8774c98b51ad977f
gwruck <gwruck@gntprojectsolutions.com.au>
2022-10-22 15:19:40 +1000

    Initial commit
    Initial commit

---
+++ BlazorApp7/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorApp7
+{
+    public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+    {
+        private ClaimsPrincipal claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity());
+
+        public override async Task<AuthenticationState> GetAuthenticationStateAsync()
+        {
+            var anonymous = new ClaimsIdentity();
+            await Task.FromResult(0);
+            return new AuthenticationState(claimsPrincipal);
+
+        }
+    }
+}
---
+++ BlazorApp7/Program.cs
@@ -0,0 +1,22 @@
+using BlazorApp7;
+using BlazorApp7.Data;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddMicrosoftGraphClient("https://graph.microsoft.com/User.Read");
+
+builder.Services.AddMsalAuthentication(options =>
+{
+    builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+    options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
+});
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
+
+await builder.Build().RunAsync();
